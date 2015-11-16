18d5ff245179e81df87c3be6b4c7b64481a1b9a1
sunya-ch <sunyanan.ch@gmail.com>
2015-11-16 23:36:48 +0900

    update table part + Only Arp request matching forward to ARP table // Successful ping just first arp_request (still bug)

--- lib/simple_router13.rb
+++ lib/simple_router13.rb
@@ -27,9 +27,14 @@ class SimpleRouter < Trema::Controller
     add_default_classifier_forwarding_flow_entry(dpid)
     add_default_l3_rewrite_forwarding_flow_entry(dpid)
     add_default_l3_routing_forwarding_flow_entry(dpid)
+    add_default_l3_forwarding_forwarding_flow_entry(dpid)
     add_default_l2_rewrite_forwarding_flow_entry(dpid)
     add_default_arp_forwarding_flow_entry(dpid)
+
     add_default_arp_flooding_flow_entry(dpid)
+    add_default_l3_forwarding_flooding_flow_entry(dpid)
+
+    add_default_l3_routing_drop(dpid)
   end
 
   def packet_in(dpid, message)
@@ -52,7 +57,7 @@ class SimpleRouter < Trema::Controller
       @interfaces.find_by(port_number: in_port,
                           ip_address: arp_request.target_protocol_address)
     return unless interface
-
+logger.info "arp_request found interface"
       actions = [
 		NiciraRegMove.new(from: :source_mac_address,to: :destination_mac_address),
 		SetSourceMacAddress.new(interface.mac_address),
@@ -61,10 +66,10 @@ class SimpleRouter < Trema::Controller
                 NiciraRegMove.new(from: :arp_sender_hardware_address,to: :arp_target_hardware_address), 
                 SetArpSenderHardwareAddress.new(interface.mac_address),
                 SetArpSenderProtocolAddress.new(interface.ip_address),
-                SendOutPort.new(in_port)
+                SendOutPort.new(:in_port)
                ]
       send_flow_mod_add(dpid, table_id: ARP_RESPONDER_TABLE_ID, idle_timeout: 0,
-      priority: 2, match: Match.new(ether_type: 0x0806, Ipv4DestinationAddress: arp_request.target_protocol_address, instructions: Apply.new(actions)))
+      priority: 2, match: Match.new(ether_type: 0x0806, ip_destination_address: interface.ip_address), instructions: Apply.new(actions))
 
    send_packet_out(
       dpid,
@@ -73,16 +78,32 @@ class SimpleRouter < Trema::Controller
         source_mac: interface.mac_address,
         sender_protocol_address: arp_request.target_protocol_address,
         target_protocol_address: arp_request.sender_protocol_address
-      ).to_binary,
-      actions: SendOutPort.new(in_port))
+     ).to_binary,
+     actions: SendOutPort.new(in_port))
 
    
    end
 
   def packet_in_arp_reply(dpid, message)
+
     @arp_table.update(message.in_port,
                       message.sender_protocol_address,
                       message.source_mac)
+logger.info "arp_reply in"
+     actions = [
+		NiciraRegMove.new(from: :source_mac_address,to: :destination_mac_address),
+		SetSourceMacAddress.new(message.source_mac),
+                SetArpOperation.new(Arp::Reply::OPERATION),
+                NiciraRegMove.new(from: :arp_sender_protocol_address,to: :arp_target_protocol_address),
+                NiciraRegMove.new(from: :arp_sender_hardware_address,to: :arp_target_hardware_address), 
+                SetArpSenderHardwareAddress.new(message.source_mac),
+                SetArpSenderProtocolAddress.new(message.sender_protocol_address),
+                SendOutPort.new(:in_port)
+               ]
+      send_flow_mod_add(dpid, table_id: ARP_RESPONDER_TABLE_ID, idle_timeout: 0,
+      priority: 2, match: Match.new(ether_type: 0x0806, ip_destination_address: message.sender_protocol_address), instructions: Apply.new(actions))
+
+
     flush_unsent_packets(dpid,
                          message.data,
                          @interfaces.find_by(port_number: message.in_port))
@@ -126,17 +147,21 @@ class SimpleRouter < Trema::Controller
   end
 
   def forward(dpid, message)
+logger.info "forwarding"
     next_hop = resolve_next_hop(message.destination_ip_address)
 
     interface = @interfaces.find_by_prefix(next_hop)
     return if !interface || (interface.port_number == message.in_port)
+logger.info "found interface"
 
     arp_entry = @arp_table.lookup(next_hop)
+
     if arp_entry
+logger.info "found next hop"
        actions = [SetSourceMacAddress.new(interface.mac_address),
                   SetDestinationMacAddress.new(arp_entry.mac_address),
                   SendOutPort.new(interface.port_number)]
-      send_flow_mod_add(dpid, table_id: L3_REWRITE_TABLE_ID, match: ExactMatch.new(message), instructions: Apply.new(actions))
+      send_flow_mod_add(dpid, table_id: L3_FORWARDING_TABLE_ID, match: ExactMatch.new(message), instructions: Apply.new(actions))
       send_packet_out(dpid, raw_data: message.raw_data, actions: actions)
     else
       send_later(dpid,
@@ -166,6 +191,7 @@ class SimpleRouter < Trema::Controller
   end
 
   def send_later(dpid, options)
+logger.info "send later"
     destination_ip = options.fetch(:destination_ip)
     @unresolved_packet_queue[destination_ip] += [options.fetch(:data)]
     send_arp_request(dpid, destination_ip, options.fetch(:interface))
@@ -184,6 +210,7 @@ class SimpleRouter < Trema::Controller
   end
 
   def send_arp_request(dpid, destination_ip, interface)
+logger.info "send request"
     arp_request =
       Arp::Request.new(source_mac: interface.mac_address,
                        sender_protocol_address: interface.ip_address,
@@ -200,7 +227,7 @@ class SimpleRouter < Trema::Controller
       table_id: CLASSIFIER_TABLE_ID,
       idle_timeout: 0,
       priority: 1000,
-      match: Match.new(dl_type: 0x0806),
+      match: Match.new(ether_type: 0x0806, arp_operation: Arp::Request::OPERATION),
       instructions: GotoTable.new(ARP_RESPONDER_TABLE_ID)
     )
 # Send L3 traffic to L3 Rewrite Table
@@ -239,7 +266,17 @@ class SimpleRouter < Trema::Controller
       dpid,
       table_id: L3_ROUTING_TABLE_ID,
       idle_timeout: 0,
-      priority: 65535,
+      match: Match.new(dl_type: 0x0800),#to_do
+      instructions: GotoTable.new(L3_FORWARDING_TABLE_ID)
+   )
+  end
+
+  def add_default_l3_forwarding_forwarding_flow_entry(dpid)
+    send_flow_mod_add(
+      dpid,
+      table_id: L3_FORWARDING_TABLE_ID,
+      idle_timeout: 0,
+      priority: 0,
       match: Match.new(dl_type: 0x0800),#to_do
       instructions: GotoTable.new(L2_REWRITE_TABLE_ID)
    )
@@ -261,7 +298,7 @@ class SimpleRouter < Trema::Controller
    #   dpid,
    #   table_id: ARP_RESPONDER_TABLE_ID,
    #   idle_timeout: 0,
-   #   priority: 1,
+   #   priority: 0,
    #   match: Match.new,
    #   instructions: Apply.new(resubmit(,L2_REWRITE_TABLE_ID))
    # )
@@ -278,4 +315,25 @@ class SimpleRouter < Trema::Controller
     )
   end
 
+  def add_default_l3_forwarding_flooding_flow_entry(dpid)
+    send_flow_mod_add(
+      dpid,
+      table_id: L3_FORWARDING_TABLE_ID,
+      idle_timeout: 0,
+      priority: 1,
+      match: Match.new,
+      instructions: Apply.new(SendOutPort.new(:controller))
+    )
+  end
+
+  def add_default_l3_routing_drop(dpid)
+# Explicit drop if cannot route
+    send_flow_mod_delete(
+      dpid,
+      table_id: L3_ROUTING_TABLE_ID,
+      idle_timeout: 0,
+      priority: 0,
+      match: Match.new
+    )
+  end
 end
